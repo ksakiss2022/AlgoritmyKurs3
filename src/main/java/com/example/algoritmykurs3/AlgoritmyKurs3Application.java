ff4815e4284eb6a6e5cff0784d29a4332ab04b58
Swetlana <sweta_zaika@mail.ru>
2022-12-25 21:23:57 +0300

    Initial commit

---
+++ src/main/java/com/example/algoritmykurs3/AlgoritmyKurs3Application.java
@@ -0,0 +1,13 @@
+package com.example.algoritmykurs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmyKurs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoritmyKurs3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/algoritmykurs3/AlgoritmyKurs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algoritmykurs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoritmyKurs3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
